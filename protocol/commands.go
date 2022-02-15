b469191e270687bfb0494af87d28ed834652f584
GabrielCRZ12 <gabriel.cruz@tem.ind.br>
2022-02-15 09:41:10 -0300

    AAA

--- protocol/commands.go
+++ protocol/commands.go
@@ -302,7 +302,7 @@ const (
 func (id DeviceID) String() string {
 	switch id {
 	case DeviceN76E003:
-		return "N76E003"
+		return "MS51FB9AE"
 	default:
 		return fmt.Sprintf("0x%08x", uint32(id))
 	}
